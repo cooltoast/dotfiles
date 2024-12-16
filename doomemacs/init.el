49ca346665bf008af8d53fa36913aaf269d0989d
cooltoast <mpatel678@gmail.com>
2024-12-15 21:10:23 -0800

    add setup steps for pylsp

--- doomemacs/init.el
+++ doomemacs/init.el
@@ -158,7 +158,10 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
+
        python            ; beautiful is better than ugly
+       ;; ^ Need to pip install 'python-lsp-server[all]' pylsp-mypy
+
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
